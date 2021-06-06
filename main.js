20073b3a9f9c8040bba366d05b818af38e6ca532
zhihaofans <zhihaofans@hotmail.com>
2021-06-06 23:28:50 +0800

    pref(main): add trycatch

--- main.js
+++ main.js
@@ -1,2 +1,19 @@
-const app = require("./scripts/app");
-app.run();
+try {
+  const app = require("./scripts/app");
+  app.run();
+} catch (error) {
+  $console.error(error);
+  $ui.alert({
+    title: error.name,
+    message: error.message,
+    actions: [
+      {
+        title: "EXIT",
+        disabled: false, // Optional
+        handler: function () {
+          $app.close();
+        }
+      }
+    ]
+  });
+}
