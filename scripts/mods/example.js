e2aa3c897d8d3da6d960dccb3ef69d8fc094ea53
zhihaofans <zhihaofans@hotmail.com>
2021-06-27 13:12:33 +0800

    update: update example.js

--- scripts/mods/example.js
+++ scripts/mods/example.js
@@ -2,8 +2,30 @@ const { Core } = require("../../Core.js/core"),
   uiKit = require("../../Core.js/ui"),
   listKit = new uiKit.ListKit();
 class Main {
-  constructor(name) {
-    this.NAME = name;
+  constructor(core) {
+    this.core = core;
+    this.kernel = core.kernel;
+    this.http = new core.$_.Http();
+  }
+  init() {
+    const main_view_list = ["example 1"],
+      didSelect = (sender, indexPath, data) => {
+        switch (indexPath.row) {
+          default:
+            $ui.alert({
+              title: indexPath.row,
+              message: data,
+              actions: [
+                {
+                  title: "OK",
+                  disabled: false, // Optional
+                  handler: function () {}
+                }
+              ]
+            });
+        }
+      };
+    listKit.pushString(this.core.MOD_NAME, main_view_list, didSelect);
   }
 }
 
@@ -14,14 +36,16 @@ class Example extends Core {
       mod_name: "例子",
       version: "1",
       author: "zhihaofans",
-      need_database: true,
+      need_database: false,
       need_core_version: 1,
       database_id: "example"
     });
     this.kernel = kernel;
   }
   run() {
-    $ui.success("run")
+    $ui.success("run");
+    const main = new Main(this);
+    main.init();
   }
 }
 module.exports = Example;
