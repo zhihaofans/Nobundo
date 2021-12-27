fdcece952470a180447fe7bd4d1962d07eb952ad
zhihaofans <zhihaofans@hotmail.com>
2021-12-27 13:51:48 +0800

    update storage.js

--- Core.js/storage.js
+++ Core.js/storage.js
@@ -14,17 +14,25 @@ class Keychain {
   constructor(domain) {
     this.DOMAIN = domain.toLowerCase();
   }
+  get(key) {
+    return $keychain.get(key, this.DOMAIN);
+  }
+  set(key, value) {
+    return $keychain.set(key, value, this.DOMAIN);
+  }
   getValue(key) {
     return $keychain.get(key, this.DOMAIN);
   }
   setValue(key, value) {
     return $keychain.set(key, value, this.DOMAIN);
   }
-  getAll(){
-    const keys=$keychain.keys(),result={
-      
-    }
-    return 
+  getAll() {
+    const keys = $keychain.keys(),
+      result = {};
+    keys.map(key => {
+      result[key] = $keychain.get(key, this.DOMAIN);
+    });
+    return result;
   }
 }
 
