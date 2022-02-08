1bcbdec9ff6cd032e8b4f678cae519a8ada950dc
zhihaofans <zhihaofans@hotmail.com>
2022-02-08 13:46:41 +0800

    更新主文件

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 try {
   require("./scripts/app").run();
 } catch (error) {
-  $console.error(error);
+  $console.error({ error });
   $ui.alert({
     title: "app.run error",
-    message: `[${error.name}]${error.message}`,
+    message: { name: error.name, message: error.message },
     actions: [
       {
         title: "EXIT",
         disabled: false, // Optional
-        handler: function () {
+        handler: () => {
           $app.close();
         }
       }
