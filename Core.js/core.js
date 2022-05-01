e7fd49bfd828bd6de39c4a97c656aad39f1f4106
zhihaofans <zhihaofans@hotmail.com>
2022-05-01 10:22:37 +0800

    更新Core.js

--- Core.js/core.js
+++ Core.js/core.js
@@ -256,6 +256,9 @@ class ModuleLoader {
         });
         return false;
       }
+      if ((thisModule.AUTHOR = undefined || thisModule.AUTHOR.length <= 0)) {
+        thisModule.AUTHOR = this.Core.CORE_INFO.AUTHOR;
+      }
       this.ModuleList[thisModule.MODULE_ID] = thisModule;
       $console.info(
         `Mod[${this.Core.CORE_INFO.NAME}]加载module[${thisModule.MODULE_NAME}]`
