bcdd066dcf1cb79fae0ac674f0c5348a034cd056
zhihaofans <zhihaofans@hotmail.com>
2022-05-31 12:57:19 +0800

    更新么方运动

--- scripts/mods/mefang.js
+++ scripts/mods/mefang.js
@@ -1,9 +1,17 @@
-const ModCore = require("../../Core.js/core").Core,
-  $ = require("../../Core.js/$");
+const ModCore = require("../../Core.js/core").Core;
 class UserData {
   constructor(core) {
     this.Core = core;
     this.Keychain = core.Keychain;
+  }
+  getHeader() {
+    return {
+      "Referer":
+        "https://servicewechat.com/wxeee6ace100d9a29a/99/page-frame.html",
+      "User-Agent": `Mozilla/5.0 (iPhone; CPU iPhone OS 15_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.22(0x18001626) NetType/4G Language/zh_CN`,
+      "token": this.getToken(),
+      "tkid": this.getTkid()
+    };
   }
   getTkid() {
     return this.Keychain.get("tkid");
@@ -11,20 +19,26 @@ class UserData {
   getToken() {
     return this.Keychain.get("token");
   }
-  setTkid(tkid) {
-    return this.Keychain.set("tkid", tkid);
-  }
-  setToken(token) {
-    return this.Keychain.set("token", token);
-  }
   inputToken() {
     $input.text({
       type: $kbType.text,
-      placeholder: "",
-      text: "",
+      placeholder: "token",
+      text: this.getToken(),
       handler: token => {
         if (token.length > 0) {
           $console.info(this.setToken(token));
+        } else if (this.getToken() > 0) {
+          $ui.alert({
+            title: "",
+            message: "",
+            actions: [
+              {
+                title: "OK",
+                disabled: false, // Optional
+                handler: () => {}
+              }
+            ]
+          });
         }
       }
     });
@@ -32,14 +46,20 @@ class UserData {
   inputTkid() {
     $input.text({
       type: $kbType.text,
-      placeholder: "",
-      text: "",
+      placeholder: "tikd",
+      text: this.getTkid(),
       handler: tkid => {
         if (tkid.length > 0) {
           $console.info(this.setTkid(tkid));
         }
       }
     });
+  }
+  setTkid(tkid) {
+    return this.Keychain.set("tkid", tkid);
+  }
+  setToken(token) {
+    return this.Keychain.set("token", token);
   }
 }
 
@@ -48,18 +68,42 @@ class MefangApi {
     this.Http = $.http;
     this.UserData = new UserData(core);
   }
-  getHeader() {
-    return {
-      "Referer":
-        "https://servicewechat.com/wxeee6ace100d9a29a/98/page-frame.html",
-      "User-Agent": `Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/8.0.18(0x1800122f) NetType/WIFI Language/zh_CN`,
-      "token": this.UserData.getToken(),
-      "tkid": this.UserData.getTkid()
-    };
+  checkLoginStatus() {
+    const tkid = this.UserData.getTkid(),
+      token = this.UserData.getToken();
+    if (tkid == undefined || token == undefined) {
+      return false;
+    }
+  }
+  async getAllTimesheet({ dateStart, dateEnd, coachId }) {
+    const result = await this.getFightBox({
+      a: "batch/timesheet",
+      d: {
+        date_start: "2022-05-28",
+        date_end: "2022-05-28",
+        coach_id: "251"
+      }
+    });
+    $console.info(result);
+  }
+  async getCommon({ a, d }) {
+    const url = "https://aifendou.net/common",
+      header = this.UserData.getHeader(),
+      body = { a, d },
+      resp = await this.Http.post({ url, body, header }),
+      response = resp.response,
+      data = resp.data;
+    $ui.loading(false);
+    if (resp.error) {
+      $console.error(resp.error);
+      return undefined;
+    } else {
+      return data;
+    }
   }
   async getFightBox({ a, d }) {
     const url = "https://aifendou.net/fightbox",
-      header = this.getHeader(),
+      header = this.UserData.getHeader(),
       body = { a, d },
       resp = await this.Http.post({ url, body, header }),
       response = resp.response,
@@ -77,42 +121,45 @@ class MefangApi {
     const result = await this.getFightBox({
       a: "timesheet",
       d: {
-        date_start: dateStart || "2022-03-01",
-        date_end: dateEnd || "2022-03-07",
+        date_start: dateStart || "2022-05-28",
+        date_end: dateEnd || "2022-05-28",
         coach_id: coachId || "251"
       }
     });
     $console.info(result);
     return result;
   }
-  async getAllTimesheet({ dateStart, dateEnd, coachId }) {
-    const result = await this.getFightBox({
-      a: "batch/timesheet",
+  async getUserData() {
+    const result = await this.getCommon({
+      a: "user/get",
       d: {
-        date_start: "2022-03-01",
-        date_end: "2022-03-07",
-        coach_id: "251"
+        who: ""
       }
     });
-    $console.info(result);
+    if (result.errcode != 0) {
+      $console.error(result.meno);
+      return undefined;
+    }
+    $console.info(result.result);
+    return result.result;
   }
 }
 
 class MefangUi {
   constructor(core) {
     this.Core = core;
+    this.$ = core.$;
     this.Api = new MefangApi(core);
   }
-  getCurriculum() {
-    const api = new MefangApi(this.Core),
-      result = api.getReservedTimesheet();
-  }
   init() {
-    $console.warn({
-      token: this.Api.UserData.getToken(),
-      tkid: this.Api.UserData.getTkid()
-    });
-    const menuList = ["输入token", "输入tkid", "获取课程", "课程二维码"];
+    const menuList = [
+      "输入token",
+      "输入tkid",
+      "获取课程",
+      "课程二维码",
+      "获取用户信息",
+      "查看教练排班"
+    ];
     $ui.menu({
       items: menuList,
       handler: (title, idx) => {
@@ -127,12 +174,122 @@ class MefangUi {
             case 2:
               this.getCurriculum();
               break;
+            case 4:
+              this.showUserData();
+              break;
+            case 5:
+              this.showCoachTimesheet();
+              break;
           }
         } catch (error) {
           $console.error(error);
         }
       }
     });
+  }
+  async showCoachTimesheet() {
+    $ui.loading(true);
+    const pickedDate = await this.$.dateTime.pickDate(),
+      date = `${pickedDate.getFullYear()}-${pickedDate.getMonth()}-${pickedDate.getDay()}`,
+      resultData = await this.Api.getReservedTimesheet("251", date, date);
+
+    $ui.loading(false);
+    $console.info({
+      date,
+      resultData
+    });
+  }
+  async showUserData() {
+    $ui.loading(true);
+    const userData = await this.Api.getUserData();
+    $ui.loading(false);
+    if (userData == undefined) {
+      $ui.error("获取失败");
+    } else if (userData.success) {
+      const userDataList = [
+        {
+          title: "姓名",
+          value: userData.name
+        },
+
+        {
+          title: "phone",
+          value: userData.phone
+        },
+        {
+          title: "avatar",
+          value: userData.avatar,
+          onClick: value => {
+            $ui.preview({
+              title: "头像",
+              url: value
+            });
+          }
+        },
+        {
+          title: "session key",
+          value: userData.session_key
+        },
+        {
+          title: "unionid",
+          value: userData.unionid
+        },
+        {
+          title: "uid",
+          value: userData.uid
+        },
+        {
+          title: "mini_openid",
+          value: userData.mini_openid
+        },
+        {
+          title: "created_at",
+          value: userData.created_at
+        },
+        {
+          title: "updated_at",
+          value: userData.updated_at
+        }
+      ];
+      $console.warn(userDataList);
+      $ui.push({
+        props: {
+          title: "用户信息"
+        },
+        views: [
+          {
+            type: "list",
+            props: {
+              data: userDataList.map(item => {
+                return {
+                  title: item.title,
+                  rows: [item.value]
+                };
+              })
+            },
+            layout: $layout.fill,
+            events: {
+              didSelect: (sender, indexPath, data) => {
+                const section = indexPath.section,
+                  row = indexPath.row,
+                  thisItem = userDataList[section];
+                if ($.isFunction(thisItem.onClick)) {
+                  try {
+                    thisItem.onClick(data);
+                  } catch (error) {
+                    $console.error(error);
+                  }
+                } else {
+                  $share.sheet([thisItem.value]);
+                }
+              }
+            }
+          }
+        ]
+      });
+    } else {
+      $ui.error("请登录");
+    }
   }
 }
 
@@ -144,7 +301,7 @@ class Mefang extends ModCore {
       modName: "么方运动",
       version: "1",
       author: "zhihaofans",
-      needCoreVersion: 4
+      coreVersion: 5
     });
     this.ui = new MefangUi(this);
   }
