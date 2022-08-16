60c9c50ce2d6633d2d96de0a5bccf4c6d9d57d8f
zhihaofans <zhihaofans@hotmail.com>
2022-08-16 10:04:32 +0800

    优化代码，移除无用mod

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,5 @@
 const AppKernel = require("../Core.js/app"),
   { ModLoader } = require("../Core.js/core"),
-  ui = require("../Core.js/ui"),
-  listKit = new ui.ListKit(),
   coreModList = [
     "bilibili.js",
     "jsbox_version.js",
@@ -9,7 +7,6 @@ const AppKernel = require("../Core.js/app"),
     "downloader.js",
     "reminder.js",
     "daymaster.js",
-    "free_api.js",
     "example.js",
     "mefang.js",
     "action_extension.js",
@@ -34,39 +31,11 @@ class App extends AppKernel {
     }
   }
   initModList() {
-    this.modLoader.addCoreByList(coreModList);
-    switch (true) {
-      case this.isKeyboardEnv():
-        $ui.render({
-          props: {
-            title: ""
-          },
-          views: [
-            {
-              type: "list",
-              props: {
-                data: [
-                  {
-                    title: "键盘模式待完善",
-                    rows: ["test"]
-                  }
-                ]
-              },
-              layout: $layout.fill,
-              events: {
-                didSelect: (_sender, indexPath, _data) => {
-                  const row = indexPath.row;
-                }
-              }
-            }
-          ]
-        });
-        break;
-      default:
-        this.modLoader.setWidgetMod("example");
-        this.modLoader.setContextMod("action_extension");
-        this.modLoader.autoRunMod();
-    }
+    this.modLoader.addModsByList(coreModList);
+    this.modLoader.setKeyboardMod("keyboard");
+    this.modLoader.setWidgetMod("example");
+    this.modLoader.setContextMod("action_extension");
+    this.modLoader.autoRunMod();
   }
 }
 function run() {
