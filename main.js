57d6d93ac75ac0ee8f82c528bb64492cb8ee518e
zhihaofans <zhihaofans@hotmail.com>
2022-09-04 11:50:35 +0800

    新增对外部库的版本检查

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 try {
-  require("./scripts/app").run();
+  let allLibNotNeedUpdate = true;
+  const libCheckResult = libVersionCheck();
+  $console.info(libCheckResult);
+  Object.keys(libCheckResult).map(libId => {
+    if (libCheckResult[libId] == false) {
+      allLibNotNeedUpdate = false;
+    }
+  });
+  if (allLibNotNeedUpdate == true) {
+    require("./scripts/app").run();
+  } else {
+    $ui.alert({
+      title: "外部库需要更新",
+      message: JSON.stringify(libCheckResult),
+      actions: [
+        {
+          title: "Exit",
+          disabled: false, // Optional
+          handler: () => {
+            $app.close();
+          }
+        }
+      ]
+    });
+  }
 } catch (error) {
   $console.error(error);
   $ui.alert({
@@ -15,4 +39,17 @@ try {
       }
     ]
   });
+}
+function libVersionCheck() {
+  const libList = { CoreJS: 8, $: 1, Next: 1 },
+    checkResult = {};
+  Object.keys(libList).map(libId => {
+    const lib = require(libId);
+    if (lib == undefined) {
+      checkResult[libId] = false;
+    } else {
+      checkResult[libId] = lib.VERSION == libList[libId];
+    }
+  });
+  return checkResult;
 }
