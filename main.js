a9d9b0df9a90b1b233f1f7a83e1a4d4355e93349
zhihaofans <zhihaofans@hotmail.com>
2022-09-06 14:45:46 +0800

    适配新版Core.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ try {
   });
 }
 function libVersionCheck() {
-  const libList = { CoreJS: 8, $: 1, Next: 1 },
+  const libList = { CoreJS: 9, $: 1, Next: 1 },
     checkResult = {};
   Object.keys(libList).map(libId => {
     const lib = require(libId);
