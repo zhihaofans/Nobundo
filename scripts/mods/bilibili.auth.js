703928c20e4a58b923c48d26b302463ed2f0c3c6
zhihaofans <zhihaofans@hotmail.com>
2025-11-11 19:31:23 +0800

    新增getCSRF

--- scripts/mods/bilibili.auth.js
+++ scripts/mods/bilibili.auth.js
@@ -1,22 +1,43 @@
 const { ModModule } = require("CoreJS"),
   $ = require("$");
+function cookieToObject(cookieStr) {
+  const cookieParts = cookieStr.split(/,(?=\s*[a-zA-Z0-9_-]+=)/);
 
+  const cookies = {};
+  for (const part of cookieParts) {
+    const match = part.match(/^\s*([^=]+)=([^;]+)/);
+    if (match) {
+      const key = match[1].trim();
+      const value = match[2].trim();
+      cookies[key] = value;
+    }
+  }
+  return cookies;
+}
 class AuthData {
   constructor(KeychainKit) {
     this.Keychain = KeychainKit;
     this.data_key = {
       cookie: "bilibili.cookie",
+      csrf: "bilibili.csrf",
       SESSDATA: "bilibili.cookie.sessdata"
     };
-  }
-  isLogin() {
-    return $.hasString(this.getCookie()) && $.hasString(this.getSESSDATA());
   }
   getCookie() {
     return this.Keychain.get(this.data_key.cookie);
   }
+  getCsrf() {
+    const cookieJson = cookieToObject(this.getCookie());
+    $console.info({
+      cookieJson
+    });
+    return cookieJson["bili_jct"];
+  }
   getSESSDATA() {
     return this.Keychain.get(this.data_key.SESSDATA);
+  }
+  isLogin() {
+    return $.hasString(this.getCookie()) && $.hasString(this.getSESSDATA());
   }
   setCookie(cookie) {
     const cookieSu = this.Keychain.set(this.data_key.cookie, cookie);
@@ -36,9 +57,11 @@ class BiliModule extends ModModule {
     });
     this.Data = new AuthData(mod.Keychain);
   }
-
   getCookie() {
     return this.Data.getCookie();
+  }
+  getCsrf() {
+    return this.Data.getCsrf();
   }
   getSESSDATA() {
     return this.Data.getSESSDATA();
