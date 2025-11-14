cd445b9e70230abcdcc30404751f7cd77e2ca00b
zhihaofans <zhihaofans@hotmail.com>
2025-11-15 00:18:47 +0800

    新增AccessKey

--- scripts/mods/bilibili.auth.js
+++ scripts/mods/bilibili.auth.js
@@ -18,10 +18,16 @@ class AuthData {
   constructor(KeychainKit) {
     this.Keychain = KeychainKit;
     this.data_key = {
+      access_key: "bilibili.access_key",
       cookie: "bilibili.cookie",
       csrf: "bilibili.csrf",
+      DedeUserID: "bilibili.dede_user_id",
+      DedeUserID__ckMd5: "bilibili.dede_user_id_ckmd5",
       SESSDATA: "bilibili.cookie.sessdata"
     };
+  }
+  getAccessKey() {
+    return this.Keychain.get(this.data_key.access_key);
   }
   getCookie() {
     return this.Keychain.get(this.data_key.cookie);
@@ -33,11 +39,29 @@ class AuthData {
     });
     return cookieJson["bili_jct"];
   }
+  getDedeUserID() {
+    const cookieJson = cookieToObject(this.getCookie());
+    $console.info({
+      cookieJson
+    });
+    return cookieJson["DedeUserID"];
+  }
+  getDedeUserID__ckMd5() {
+    const cookieJson = cookieToObject(this.getCookie());
+    $console.info({
+      cookieJson
+    });
+    return cookieJson["DedeUserID__ckMd5"];
+  }
   getSESSDATA() {
     return this.Keychain.get(this.data_key.SESSDATA);
   }
   isLogin() {
     return $.hasString(this.getCookie()) && $.hasString(this.getSESSDATA());
+  }
+  setAccess_key(access_key) {
+    const su = this.Keychain.set(this.data_key.access_key, access_key);
+    return su;
   }
   setCookie(cookie) {
     const cookieSu = this.Keychain.set(this.data_key.cookie, cookie);
@@ -57,17 +81,29 @@ class BiliModule extends ModModule {
     });
     this.Data = new AuthData(mod.Keychain);
   }
+  getAccessKey() {
+    return this.Data.getAccessKey();
+  }
   getCookie() {
     return this.Data.getCookie();
   }
   getCsrf() {
     return this.Data.getCsrf();
   }
+  getDedeUserID() {
+    return this.Data.getDedeUserID();
+  }
+  getDedeUserID__ckMd5() {
+    return this.Data.getDedeUserID__ckMd5();
+  }
   getSESSDATA() {
     return this.Data.getSESSDATA();
   }
   isLogin() {
     return this.Data.isLogin();
+  }
+  setAccessKey(access_key) {
+    return this.Data.setAccess_key(access_key);
   }
   setCookie(cookie) {
     return this.Data.setCookie(cookie);
