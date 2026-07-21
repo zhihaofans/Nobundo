5a6d3059721a140cfaf0c14d6b8b8f14145624f3
zhihaofans <zhihaofans@hotmail.com>
2026-07-21 10:35:08 +0800

    remove mods

--- scripts/mods.js
+++ scripts/mods.js
@@ -3,23 +3,20 @@ module.exports = {
   mods: [
     "jsbox_version.js",
     "downloader.js",
-    "daymaster.js",
     "example.js",
     "action_extension.js",
     "datacenter.js",
     "content_box.js",
     "keyboard.js",
-    "network_api.js",
     "mod_manager.js",
     "web_browser.js",
     "clipboard.js",
-    "scoop.js",
     "viewer.js",
-    "jsdelivr.js",
     "search.js",
-    "tikhub.js",
     "config.js",
-    "bilibili.js",
-    "qrcode.js"
+    "qrcode.js",
+    "webparse.js",
+    "relata_vault.js",
+    "webclip.js"
   ]
 };
